a357dc21412ae31001e03969c6916651b4dae616
rinuya <91641202+rinuya@users.noreply.github.com>
2022-01-23 20:29:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const operate = function(x, operator, y){
     switch(operator){
         case "/"    :
@@ -8,24 +9,75 @@ const operate = function(x, operator, y){
             return add(x,y);
         case "-"     :
             return subtract(x,y);
-}
-}
+}}
 
 
+let equation = ""; 
+const display = document.querySelector("#display");
+//QUESTION: WHY DO I NEED TO ADD "event" in the addEventListener function and later in the "event.target.textContent"
+const btn = document.getElementsByClassName("btn")
+for (i=0; i<btn.length; i++){
+    btn[i].addEventListener("click", event => {
+        equation += event.target.textContent;
+        display.innerText = equation;})}
+
+//eventlistener for =
+const equal = document.querySelector(".equal");
+equal.addEventListener("click", ()=>{
+    calculation();});
+
+//calculation of string
+const calculation = function (){
+    
+    //REGEX function for filtering needed, + and * need a backslash. others can go in brackets
+    // let arr_equ = equation.split(/[\+\*/-]/g);
+    let arr_equ = equation.split(" ");
+    for (let i=0; i<arr_equ.length; i++){
+        if (arr_equ[i]==="*" || arr_equ[i]==="/"){
+            let temp = operate(arr_equ[i-1], arr_equ[i], arr_equ[i+1]);
+            arr_equ[i-1] = temp;
+            arr_equ.splice(i,2);
+            i=0;    
+        }
+    }
+    for (i=0; i<arr_equ.length; i++){
+        if (arr_equ[i]=="+" || arr_equ[i]=="-"){
+            var temp2 = operate(arr_equ[i-1], arr_equ[i], arr_equ[i+1]);
+            arr_equ[i-1] = temp2;
+            arr_equ.splice(i,2)
+            i=0;}
+    }
+    result = Math.round(arr_equ*1000)/1000
+    display.innerHTML= equation + " = " + result;
+}
+
+//clear button
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", ()=>{
+    equation = "";
+    result="";
+    display.innerHTML="";
+});
+
+//delete button
+const delete1 = document.querySelector(".delete1");
+delete1.addEventListener("click", ()=>{
+    equation=equation.substring(0, equation.length -1)
+    display.innerHTML= equation;
+});
+
+
+
+
+//suboperations for function operate(); might be unnecessary
 const add = function(x, y) {
-	return x+y;
+	return Number(x)+Number(y);
 };
 
 const subtract = function(x,y) {
 	return x-y;
 };
 
-// const sum = function(array){
-//   return array.reduce((u, y) =>  u + y, 0);
-// };
-// const multiply = function(array) {
-//   return array.reduce((x,y) => x*y);
-// };
 const multiply = function(x,y){
     return x*y;
 }
@@ -33,7 +85,7 @@ const multiply = function(x,y){
 const divide = function(x,y){
     return x/y;
 };
-
+//not being used
 const power = function(num1,times) {
   let result = 1;
   for (i = 0; i<times; i++){
